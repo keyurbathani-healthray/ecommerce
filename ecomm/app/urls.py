2727242c5ad6e8ca5c9aaeb1696e8780398cb75b
keyurbathani-healthray <keyurbathani.healthray@gmail.com>
2025-10-14 18:17:19 +0530

    category wise items and items details

--- ecomm/app/urls.py
+++ ecomm/app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
    path('', views.home, name='home'),
-]  
+   path('category/<slug:val>/', views.CategoryView.as_view(), name='category'),
+   path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_details'),
+
+   ]  
--- ecomm/ecomm/urls.py
+++ ecomm/ecomm/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +26,4 @@ urlpatterns = [
 
     path("__reload__/", include("django_browser_reload.urls")),
 
-] 
-
-# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
