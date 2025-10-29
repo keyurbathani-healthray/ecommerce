5cbb51947c5defa5664e80995aa7b2ef3393c943
keyurbathani-healthray <keyurbathani.healthray@gmail.com>
2025-10-29 12:24:29 +0530

    changes

--- ecomm/app/urls.py
+++ ecomm/app/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
    path('category/<slug:val>/', views.CategoryView.as_view(), name='category'),
    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_details'),
    path('category-title/<val>/', views.CategoryTitleView.as_view(), name='category-title'),
+
+
+   # Authentication URLs
    path('register/', views.customer_registration, name='customer-registration'),
    path('login/', views.customer_login, name='customer-login'),
    path('logout/', views.customer_logout, name='customer-logout'),
